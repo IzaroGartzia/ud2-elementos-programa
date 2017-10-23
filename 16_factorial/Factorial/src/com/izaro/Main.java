d598acda7d2a3db598aa8906925322cf42dff6e1
Izaro <izaro.garcia.uribe@gmail.com>
2017-10-23 20:44:07 +0200

    Ejercicio 16

---
+++ 16_factorial/Factorial/src/com/izaro/Main.java
@@ -0,0 +1,22 @@
+package com.izaro;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int respuesta = 1;
+        System.out.println("Escribe un número: ");
+        int a = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= a; i++) {
+            respuesta = i * respuesta;
+            System.out.println(respuesta);
+
+        }
+    }
+}
